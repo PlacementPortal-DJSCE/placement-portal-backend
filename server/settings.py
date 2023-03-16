63dc881bc72a6ca90165aa252693f037f6581f94
chaitanya_1911 <chaitanya.kumbhar@think360.ai>
2023-03-16 19:48:01 +0530

    added media url

--- server/settings.py
+++ server/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 CSRF_COOKIE_NAME = "csrftoken"
 CORS_ALLOW_CREDENTIALS = True
