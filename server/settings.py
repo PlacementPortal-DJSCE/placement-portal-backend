99280ebbeb1dd6ada93d67adde8f3b16380f0392
chaitanya_1911 <chaitanya.kumbhar@think360.ai>
2023-03-17 15:04:08 +0530

    added allowed host

--- server/settings.py
+++ server/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
-#ALLOWED_HOSTS = ["http://localhost:3000/",os.environ.get("FRONT_END_LINK")]
+#ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost",'127.0.0.1',os.environ.get("FRONT_END_HOST")]
 
 
 # Application definition
